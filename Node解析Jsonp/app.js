77f6f42fbc200be80bd1fb7cd7f713a4a210f04e
tiemao <1778900272@qq.com>
2019-06-20 19:06:14 +0800

    20190620

--- Node解析Jsonp/app.js
+++ Node解析Jsonp/app.js
@@ -4,11 +4,7 @@ const fs = require('fs')
 const querystring = require('querystring')
 
 //创建一个http服务器
-const server = http.createServer()
-
-
-//监听http服务器的request请求
-server.on('request',(req,res) =>{
+http.createServer((req,res) =>{
     //简单粗暴解决二跨域问题，允许所有的域进行访问
     res.writeHead(200,{
         'Content-Type': 'text/plain',
@@ -31,29 +27,27 @@ server.on('request',(req,res) =>{
             }
         })
     }else if(url === '/add'){
+            console.log("add")
             var body = '';
             //// 通过req的data事件监听函数，每当接受到请求体的数据，就累加到body变量中
-            req.on('data', function (data) {
+            req.on('data', function (data) {               
                 body += data;
             });
 
             req.on('end', function () {
-                var myBody = querystring.parse(body); //querystring.parse将post解析为真正的POST请求格式
-                // res.write(JSON.stringify(myBody));
-                // fs.writeFile('data.txt',JSON.stringify(myBody),(err)=>{
-                //     if(err){
-                //         console.log(err)
-                //     }else{
-                //         console.log("ok")
-                //     }
-                // })
-                console.log(myBody)
-                res.end(JSON.stringify(myBody));
-            })
+                console.log(body)
+                fs.writeFile('data.txt',body,(err)=>{
+                    if(err){
+                        console.log(err)
+                    }else{
+                        console.log("ok")
+                    }
+                })    
+              })
+               
     }else{
         res.end('404')
     }
-})
-server.listen(3000,()=>{
+}).listen(3000,()=>{
     console.log('server listen at http://127.0.0.1:3000')
 })
